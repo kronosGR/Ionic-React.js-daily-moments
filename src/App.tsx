7cdbae9d707bdc51ac2064bd64fa763a942655c4
kronosGR <geo.elgeo@gmail.com>
2022-05-06 06:32:52 +0200

    added usecontext for user global state

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import { IonReactRouter } from '@ionic/react-router';
 import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router';
 import AppTabs from './AppTabs';
+import { AuthContext } from './auth';
 import LoginPage from './pages/LoginPage';
 
 const App: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonRouterOutlet>
-          <Route exact path='/login'>
-            <LoginPage onLogin={() => setLoggedIn(true)} loggedIn={loggedIn} />
-          </Route>
-          <Route path='/my'>
-            <AppTabs loggedIn={loggedIn} />
-          </Route>
-          <Redirect exact path='/' to='/my/entries' />
-        </IonRouterOutlet>
-      </IonReactRouter>
+      <AuthContext.Provider value={{ loggedIn }}>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route exact path='/login'>
+              <LoginPage onLogin={() => setLoggedIn(true)} />
+            </Route>
+            <Route path='/my'>
+              <AppTabs />
+            </Route>
+            <Redirect exact path='/' to='/my/entries' />
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </AuthContext.Provider>
     </IonApp>
   );
 };
