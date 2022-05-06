7d94fbaaf719a8cee975fa3dcecc33bebd100a38
kronosGR <geo.elgeo@gmail.com>
2022-05-06 06:26:07 +0200

    separated protected pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,7 +21,7 @@ const HomePage: React.FC = () => {
       <IonContent className='ion-padding'>
         <IonList>
           {entries.map((entry) => (
-            <IonItem button key={entry.id} routerLink={`/entries/${entry.id}`}>
+            <IonItem button key={entry.id} routerLink={`/my/entries/${entry.id}`}>
               {entry.title}
             </IonItem>
           ))}
