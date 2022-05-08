ed00e62bd139d89f9363d8623c7ba7f5564bb146
kronosGR <geo.elgeo@gmail.com>
2022-05-08 21:05:40 +0200

    refactored code

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,34 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth as firebaseAuth } from './firebase';
 
-export const AuthContext = createContext({ loggedIn: false });
+interface Auth {
+  loggedIn: boolean;
+  userId?: string;
+}
 
-export function useAuth() {
+interface AuthInit {
+  loading: boolean;
+  auth?: Auth;
+}
+
+export const AuthContext = createContext<Auth>({ loggedIn: false });
+
+export function useAuth(): Auth {
   return useContext(AuthContext);
+}
+
+export function useAuthInit(): AuthInit {
+  const [authInit, setAuthInit] = useState<AuthInit>({
+    loading: true,
+  });
+
+  useEffect(() => {
+    return firebaseAuth.onAuthStateChanged((firebaseUser) => {
+      const auth = firebaseUser
+        ? { loggedIn: true, userId: firebaseUser.uid }
+        : { loggedIn: false };
+      setAuthInit({ loading: false, auth });
+    });
+  }, []);
+  return authInit;
 }
