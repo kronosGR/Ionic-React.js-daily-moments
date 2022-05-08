eea8073ef7dafd869355ba3f2fca3f3008e5c326
kronosGR <geo.elgeo@gmail.com>
2022-05-08 11:31:05 +0200

    added loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { IonApp } from '@ionic/react';
+import { IonApp, IonLoading } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import React, { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
@@ -9,17 +9,20 @@ import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 const App: React.FC = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [authState, setAuthState] = useState({ loading: true, loggedIn: false });
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setLoggedIn(Boolean(user));
+      setAuthState({ loading: false, loggedIn: Boolean(user) });
     });
   }, []);
 
+  if (authState.loading) {
+    return <IonLoading isOpen />;
+  }
   return (
     <IonApp>
-      <AuthContext.Provider value={{ loggedIn }}>
+      <AuthContext.Provider value={{ loggedIn: authState.loggedIn }}>
         <IonReactRouter>
           <Switch>
             <Route exact path='/login'>
