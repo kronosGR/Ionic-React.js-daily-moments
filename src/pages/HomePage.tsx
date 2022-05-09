e54d1f5453d3f59d60b14aee896e741a27392fac
kronosGR <geo.elgeo@gmail.com>
2022-05-09 06:31:17 +0200

    fatch data from firstore

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,10 +7,25 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import React from 'react';
-import { entries } from '../data';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebase';
 
 const HomePage: React.FC = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const entriesRef = firestore.collection('entries');
+    entriesRef.get().then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        const entries = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setEntries(entries);
+      });
+    });
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
