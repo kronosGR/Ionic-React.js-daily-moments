40381018182c79a556060c5d829f2727ec8c120b
kronosGR <geo.elgeo@gmail.com>
2022-05-11 06:19:10 +0200

    adding firestore document

--- src/pages/AddEntryPage.tsx
+++ src/pages/AddEntryPage.tsx
@@ -1,19 +1,37 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
   IonPage,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import React from 'react';
-
-interface RouteParams {
-  id: string;
-}
+import React, { useState } from 'react';
+import { firestore } from '../firebase';
+import { useAuth } from '../auth';
+import { useHistory } from 'react-router';
 
 const AddEntryPage: React.FC = () => {
+  const { userId } = useAuth();
+  const history = useHistory();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSave = async () => {
+    const entriesRef = firestore.collection('users').doc(userId).collection('entries');
+    const entryData = { title, description };
+    const entryRef = await entriesRef.add(entryData);
+    console.log('saved', entryRef.id);
+    history.goBack();
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -24,7 +42,27 @@ const AddEntryPage: React.FC = () => {
           <IonTitle>Add Entry</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>TODO</IonContent>
+      <IonContent className='ion-padding'>
+        <IonList>
+          <IonItem>
+            <IonLabel position='stacked'>Title</IonLabel>
+            <IonInput
+              value={title}
+              onIonChange={(event) => setTitle(event.detail.value)}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position='stacked'>Description</IonLabel>
+            <IonTextarea
+              value={description}
+              onIonChange={(event) => setDescription(event.detail.value)}
+            />
+          </IonItem>
+          <IonButton expand='block' onClick={handleSave}>
+            Save
+          </IonButton>
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 };
